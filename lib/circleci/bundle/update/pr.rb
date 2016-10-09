81fc80bc0db7def8a3718532c0c6690b23fa4f43
Takashi Masuda <masutaka.net@gmail.com>
2016-10-09 18:24:22 +0900

    Improve GitHub PR description

--- lib/circleci/bundle/update/pr.rb
+++ lib/circleci/bundle/update/pr.rb
@@ -55,6 +55,8 @@ module Circleci
           compare_linker.formatter = CompareLinker::Formatter::Markdown.new
 
           comment = <<-EOC
+**Updated RubyGems:**
+
 #{compare_linker.make_compare_links.to_a.join("\n")}
 
 Powered by [compare_linker](https://rubygems.org/gems/compare_linker)
