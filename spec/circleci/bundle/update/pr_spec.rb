d24fce011fe200ec002ba8754dbd499c84bc83f2
Takashi Masuda <masutaka.net@gmail.com>
2019-04-14 21:14:41 +0900

    $ rubocop -a --only RSpec/EmptyLineAfterHook

--- spec/circleci/bundle/update/pr_spec.rb
+++ spec/circleci/bundle/update/pr_spec.rb
@@ -7,16 +7,19 @@ describe Circleci::Bundle::Update::Pr do
 
     context 'given git@github.com:masutaka/compare_linker.git' do
       before { ENV['CIRCLE_REPOSITORY_URL'] = 'git@github.com:masutaka/compare_linker.git' }
+
       it { is_expected.to eq 'github.com' }
     end
 
     context 'given https://github.com/masutaka/circleci-bundle-update-pr.git' do
       before { ENV['CIRCLE_REPOSITORY_URL'] = 'https://github.com/masutaka/circleci-bundle-update-pr.git' }
+
       it { is_expected.to eq 'github.com' }
     end
 
     context 'given nil' do
       before { ENV['CIRCLE_REPOSITORY_URL'] = nil }
+
       it { is_expected.to eq 'github.com' }
     end
   end
