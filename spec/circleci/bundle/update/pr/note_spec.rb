ebc6504bca1e64f6a1f3dbc1faf0dbc5c2adba17
Takashi Masuda <masutaka.net@gmail.com>
2019-04-14 21:47:53 +0900

    :recycle: Fix RSpec/ContextWording

--- spec/circleci/bundle/update/pr/note_spec.rb
+++ spec/circleci/bundle/update/pr/note_spec.rb
@@ -19,7 +19,7 @@ describe Circleci::Bundle::Update::Pr::Note do
   describe '.exist?' do
     subject { Circleci::Bundle::Update::Pr::Note.exist? }
 
-    context 'given only .circleci/BUNDLE_UPDATE_NOTE.md' do
+    context 'with only .circleci/BUNDLE_UPDATE_NOTE.md' do
       before do
         FileUtils.touch('.circleci/BUNDLE_UPDATE_NOTE.md')
       end
@@ -27,7 +27,7 @@ describe Circleci::Bundle::Update::Pr::Note do
       it { is_expected.to be_truthy }
     end
 
-    context 'given only CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
+    context 'with only CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
       before do
         FileUtils.touch('CIRCLECI_BUNDLE_UPDATE_NOTE.md')
       end
@@ -35,7 +35,7 @@ describe Circleci::Bundle::Update::Pr::Note do
       it { is_expected.to be_truthy }
     end
 
-    context 'given .circleci/BUNDLE_UPDATE_NOTE.md and CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
+    context 'with .circleci/BUNDLE_UPDATE_NOTE.md and CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
       before do
         FileUtils.touch('.circleci/BUNDLE_UPDATE_NOTE.md')
         FileUtils.touch('CIRCLECI_BUNDLE_UPDATE_NOTE.md')
@@ -44,7 +44,7 @@ describe Circleci::Bundle::Update::Pr::Note do
       it { is_expected.to be_truthy }
     end
 
-    context 'given nothing' do
+    context 'with nothing' do
       it { is_expected.to be_falsy }
     end
   end
@@ -52,7 +52,7 @@ describe Circleci::Bundle::Update::Pr::Note do
   describe '.read' do
     subject { Circleci::Bundle::Update::Pr::Note.read }
 
-    context 'given .circleci/BUNDLE_UPDATE_NOTE.md' do
+    context 'with .circleci/BUNDLE_UPDATE_NOTE.md' do
       before do
         File.open('.circleci/BUNDLE_UPDATE_NOTE.md', 'w') { |f| f.write('I am .circleci/BUNDLE_UPDATE_NOTE.md') }
       end
@@ -60,7 +60,7 @@ describe Circleci::Bundle::Update::Pr::Note do
       it { is_expected.to eq 'I am .circleci/BUNDLE_UPDATE_NOTE.md' }
     end
 
-    context 'given CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
+    context 'with CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
       before do
         File.open('CIRCLECI_BUNDLE_UPDATE_NOTE.md', 'w') { |f| f.write('I am CIRCLECI_BUNDLE_UPDATE_NOTE.md') }
       end
@@ -68,7 +68,7 @@ describe Circleci::Bundle::Update::Pr::Note do
       it { is_expected.to eq 'I am CIRCLECI_BUNDLE_UPDATE_NOTE.md' }
     end
 
-    context 'given .circleci/BUNDLE_UPDATE_NOTE.md and CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
+    context 'with .circleci/BUNDLE_UPDATE_NOTE.md and CIRCLECI_BUNDLE_UPDATE_NOTE.md' do
       before do
         File.open('.circleci/BUNDLE_UPDATE_NOTE.md', 'w') { |f| f.write('I am .circleci/BUNDLE_UPDATE_NOTE.md') }
         File.open('CIRCLECI_BUNDLE_UPDATE_NOTE.md', 'w') { |f| f.write('I am CIRCLECI_BUNDLE_UPDATE_NOTE.md') }
@@ -77,7 +77,7 @@ describe Circleci::Bundle::Update::Pr::Note do
       it { is_expected.to eq 'I am .circleci/BUNDLE_UPDATE_NOTE.md' }
     end
 
-    context 'given nothing' do
+    context 'with nothing' do
       it { is_expected.to be_nil }
     end
   end
--- spec/circleci/bundle/update/pr_spec.rb
+++ spec/circleci/bundle/update/pr_spec.rb
@@ -6,19 +6,19 @@ describe Circleci::Bundle::Update::Pr do
 
     after { ENV['CIRCLE_REPOSITORY_URL'] = nil }
 
-    context 'given git@github.com:masutaka/compare_linker.git' do
+    context "when ENV['CIRCLE_REPOSITORY_URL'] is git@github.com:masutaka/compare_linker.git" do
       before { ENV['CIRCLE_REPOSITORY_URL'] = 'git@github.com:masutaka/compare_linker.git' }
 
       it { is_expected.to eq 'github.com' }
     end
 
-    context 'given https://github.com/masutaka/circleci-bundle-update-pr.git' do
+    context "when ENV['CIRCLE_REPOSITORY_URL'] is https://github.com/masutaka/circleci-bundle-update-pr.git" do
       before { ENV['CIRCLE_REPOSITORY_URL'] = 'https://github.com/masutaka/circleci-bundle-update-pr.git' }
 
       it { is_expected.to eq 'github.com' }
     end
 
-    context 'given nil' do
+    context "with ENV['CIRCLE_REPOSITORY_URL'] is nil" do
       before { ENV['CIRCLE_REPOSITORY_URL'] = nil }
 
       it { is_expected.to eq 'github.com' }
@@ -34,19 +34,19 @@ describe Circleci::Bundle::Update::Pr do
       )
     end
 
-    context 'running_target is included in target branches' do
+    context 'when running_target is included in target branches' do
       let(:running_branch) { 'target' }
 
       it { is_expected.to be_truthy }
     end
 
-    context "ENV['CIRCLE_BRANCH'] is not included in target branches" do
+    context "when ENV['CIRCLE_BRANCH'] is not included in target branches" do
       let(:running_branch) { 'not_included' }
 
       it { is_expected.to be_falsy }
     end
 
-    context "ENV['CIRCLE_BRANCH'] is nil" do
+    context "when ENV['CIRCLE_BRANCH'] is nil" do
       let(:running_branch) { nil }
 
       it { is_expected.to be_falsy }
