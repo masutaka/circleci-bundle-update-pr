16392050829ea7a1f565add154a9aeb04107c017
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2020-01-25 22:26:01 +0900

    refactoring

--- lib/circleci/bundle/update/pr.rb
+++ lib/circleci/bundle/update/pr.rb
@@ -241,9 +241,10 @@ Powered by [circleci-bundle-update-pr](https://rubygems.org/gems/circleci-bundle
         #
         # @return [String]
         def self.lockfile_path
-          return 'Gemfile.lock' unless ENV['CIRCLE_WORKING_DIRECTORY']
+          workdir_env = ENV['CIRCLE_WORKING_DIRECTORY']
+          return 'Gemfile.lock' unless workdir_env
 
-          workdir = Pathname.new(File.expand_path(ENV['CIRCLE_WORKING_DIRECTORY']))
+          workdir = Pathname.new(workdir_env).expand_path
           gemfile_lock = Pathname.new(File.expand_path('Gemfile.lock'))
           gemfile_lock.relative_path_from(workdir).to_s
         end
